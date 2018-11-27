de0a6ac3be630c0468ce9dd7b7b4df9018aacede
Kailin Tang <41275508+kltang96@users.noreply.github.com>
2018-11-26 21:56:57 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE GROUP_PROJECT337;
 USE GROUP_PROJECT337;
 
-CREATE TABLE customer(
-  ID varchar(10) NOT NULL UNIQUE,
+CREATE TABLE CUSTOMER(
+  customerID varchar(10) NOT NULL UNIQUE,
   email varchar(100) not null UNIQUE PRIMARY KEY,
   password varchar(30) not null, 
   Fname VARCHAR(30) NOT NULL,
@@ -14,34 +14,44 @@ CREATE TABLE customer(
   cart VARCHAR(30) not null,
 );
 
-CREATE TABLE employee(
-  ID varchar(10) NOT NULL UNIQUE,
+CREATE TABLE EMPLOYEE(
+  employeeID varchar(10) NOT NULL UNIQUE,
   email varchar(100) not null UNIQUE PRIMARY KEY,
   password varchar(30) not null, 
   Fname VARCHAR(30) NOT NULL,
   Lname VARCHAR(30) NOT NULL,
   phone VARCHAR(12) NOT NULL,
   gender ENUM('M', 'F', 'O'), 
-  DoB DATE NOT NULL,
+  DoB DATE,
   address varchar(300) not null, 
   SSN varchar(9) NOT NULL, 
   role ENUM('admin', 'employee', 'observer') not null,
 );
 
-CREATE TABLE PRODUCTS(
-productid INT not NULL UNIQUE PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
-brand VARCHAR(30) NOT NULL,
-description text(300) NOT NULL,
-CASE_COUNT CHAR(2) NOT NULL,
-CASE_PRICE FLOAT(3) NOT NULL);
+CREATE TABLE ROLES(
+  rolename VARCHAR(30) NOT NULL,
+  ch_employee ENUM('No', 'Yes'), 
+  ch_product ENUM('No', 'Yes'),
+);
 
-CREATE TABLE ORDER(
-ORDER_ID INT NOT NULL PRIMARY KEY,
-VEND_NUMBER INT  NOT NULL UNIQUE,
-FOREIGN KEY (VEND_NUMBER) REFERENCES PRODUCTS(VEND_NUMBER),
-TOTAL_COST FLOAT(2),
-ORDER_STATUS VARCHAR(30),
-ORDER_DATE TIMESTAMP,
-DELIEVERY_DATE DATE );
+CREATE TABLE PRODUCTS(
+  productid INT not NULL UNIQUE PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  brand VARCHAR(30) NOT NULL,
+  description text(300) NOT NULL,
+  picture VARCHAR(100) NOT NULL,
+  price FLOAT(24) NOT NULL
+  type ENUM('water', 'juice', 'soda', 'alcoholic', 'other'), 
+  quantity INT not NULL,
+);
+
+CREATE TABLE ORDERS(
+  orderID INT NOT NULL PRIMARY KEY,
+  customerID varchar(10) NOT NULL,
+  cost FLOAT(24),
+  status ENUM('pending', 'fulfilled', 'special'),
+  orderdate DATE,
+  fulfilldate DATE,
+  request VARCHAR(300) NOT NULL,
+);
 
